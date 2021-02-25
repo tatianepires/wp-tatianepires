9e8bd6d88866d192d762154c360730d262fce50d
Tatiane Pires <tatiane@tatianepires.net>
2021-02-25 19:13:56 -0300

    Change site-info.php

--- template-parts/footer/site-info.php
+++ template-parts/footer/site-info.php
@@ -10,11 +10,6 @@
 
 ?>
 <div class="site-info">
-	<?php
-	if ( function_exists( 'the_privacy_policy_link' ) ) {
-		the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
-	}
-	?>
 	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>" class="imprint" target="_blank">
 		<?php printf( __( 'Proudly powered by %s', 'twentyseventeen' ), 'WordPress' ); ?>
 	</a>
